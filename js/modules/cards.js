80530ea979ca89388d0f6839f47fbdf6426f01c9
Taras <tarasmikulskii@ukr.net>
2022-06-30 18:21:38 +0300

    Using ES6 modules for division into modules

--- js/modules/cards.js
+++ js/modules/cards.js
@@ -1,3 +1,5 @@
+import {getResurce} from '../services/services';
+
 function cards() {
     // Use classes for menu cards
 
@@ -42,16 +44,6 @@ function cards() {
         }
     }
 
-    const getResurce = async (url) => {
-        const res = await fetch(url);
-
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status ${res.status}`);
-        }
-
-        return await res.json();
-    };
-
     getResurce("http://localhost:3000/menu")
         .then(data => {
             data.forEach(({ img, altimg, title, descr, price }) => {
@@ -60,4 +52,4 @@ function cards() {
         });
 }
 
-module.exports = cards;
+export default cards;
